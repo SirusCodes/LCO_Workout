8aa35d46c504ad997ad302a42a6497000da2d42e
SirusCodes <darshanrander@gmail.com>
2020-04-30 21:06:58 +0530

    added images

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+const List<String> images = [
+  "arm_raise.png",
+  "back_streaching.png",
+  "ball_plank.png",
+  "declined_crunches.png",
+  "dumbel_curls.png",
+  "inclined_bench_press.png",
+  "jackknife_sit_ups.png",
+  "push_ups.png",
+  "renegade_row_pushups.png",
+  "seated_cable_row.png",
+  "vrukshansana.png",
+];
+
+const List<String> musics = [
+  "pop.mp3",
+  "rock_1.mp3",
+  "rock_2.mp3",
+  "up_from_the_ashes.mp3",
+];
--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:lco_workout/animations/fade_slide.dart';
+import 'package:lco_workout/constants.dart';
 import 'package:lco_workout/screen/sets_screen.dart';
 import 'package:neumorphic/neumorphic.dart';
 import 'package:shimmer/shimmer.dart';
@@ -14,6 +17,13 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  List<String> exerciseList = [], rawList = [];
+  @override
+  void initState() {
+    super.initState();
+    getList();
+  }
+
   @override
   Widget build(BuildContext context) {
     final _size = MediaQuery.of(context).size;
@@ -50,27 +60,27 @@ class _MainPageState extends State<MainPage> {
                   children: <Widget>[
                     FadeSlide(
                       delay: 1.5,
-                      child: buildCard(context, "1", _size),
+                      child: buildCard(context, exerciseList[0], _size),
                       leftToRight: true,
                     ),
                     FadeSlide(
                       delay: 1.7,
-                      child: buildCard(context, "2", _size),
+                      child: buildCard(context, exerciseList[1], _size),
                       leftToRight: true,
                     ),
                     FadeSlide(
                       delay: 2.0,
-                      child: buildCard(context, "3", _size),
+                      child: buildCard(context, exerciseList[2], _size),
                       leftToRight: true,
                     ),
                     FadeSlide(
                       delay: 2.3,
-                      child: buildCard(context, "4", _size),
+                      child: buildCard(context, exerciseList[3], _size),
                       leftToRight: true,
                     ),
                     FadeSlide(
                       delay: 2.6,
-                      child: buildCard(context, "5", _size),
+                      child: buildCard(context, exerciseList[4], _size),
                       leftToRight: true,
                     ),
                   ],
@@ -113,7 +123,6 @@ class _MainPageState extends State<MainPage> {
   }
 
   Padding buildCard(BuildContext context, String title, Size size) {
-    final _size = MediaQuery.of(context).size.height;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: NeuCard(
@@ -124,16 +133,35 @@ class _MainPageState extends State<MainPage> {
           borderRadius: BorderRadius.circular(8),
         ),
         child: SizedBox(
-          height: _size / 13,
+          height: size.height / 13,
           width: size.width,
           child: Center(
             child: Text(
               title,
-              style: Theme.of(context).primaryTextTheme.display1,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context)
+                  .primaryTextTheme
+                  .display1
+                  .copyWith(fontSize: size.height / 30),
             ),
           ),
         ),
       ),
     );
+  }
+
+  void getList() {
+    Random random = Random();
+    int count = 5, i;
+    while (count > 0) {
+      i = random.nextInt(5 + count);
+      var item = images[i];
+      if (!rawList.contains(item)) {
+        rawList.add(item);
+        item = item.replaceAll("_", " ").replaceFirst(".png", "").toUpperCase();
+        exerciseList.add(item);
+        count--;
+      }
+    }
   }
 }
